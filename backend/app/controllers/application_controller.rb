5edfd79bd3b5e05622cf7ce85477cfd9b6c7334b
shadiyardesk <shadiyar@deskalerts.com>
2026-01-22 22:26:20 +0500

    Fix authenticate_user! callback missing error

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
