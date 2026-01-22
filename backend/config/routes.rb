a95c2ab0c6241ec0b8ad1c3057d99581917eb5ca
shadiyardesk <shadiyar@deskalerts.com>
2026-01-22 22:36:11 +0500

    Add root route for API verification

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -50,5 +50,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: proc { [200, {}, ['API is online']] }
 end
