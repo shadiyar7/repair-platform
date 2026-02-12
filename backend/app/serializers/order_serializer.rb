f1610d9db0788ee7e90072f4830284ad35cdda75
shadiyardesk <shadiyar@deskalerts.com>
2026-02-12 23:07:58 +0500

    feat(api): expose invoice_base64 in order serializer

--- backend/app/serializers/order_serializer.rb
+++ backend/app/serializers/order_serializer.rb
@@ -2,7 +2,7 @@ class OrderSerializer
   include JSONAPI::Serializer
   attributes :status, :total_amount, :delivery_address, :delivery_notes, :created_at, :order_items,
              :driver_name, :driver_phone, :driver_car_number, :driver_arrival_time, 
-             :director_signed_at, :smart_link_token
+             :director_signed_at, :smart_link_token, :invoice_base64
 
   attribute :order_items do |object|
     object.order_items.map do |item|
