0859a0020a89b13041d581d8caf9992cb3259fbc
shadiyardesk <shadiyar@deskalerts.com>
2026-02-15 13:39:09 +0500

    Fix: Allow null warehouse_id in users table

--- backend/db/migrate/20260215083244_add_warehouse_ref_to_users.rb
+++ backend/db/migrate/20260215083244_add_warehouse_ref_to_users.rb
@@ -1,5 +1,5 @@
 class AddWarehouseRefToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_reference :users, :warehouse, null: false, foreign_key: true
+    add_reference :users, :warehouse, null: true, foreign_key: true
   end
 end
