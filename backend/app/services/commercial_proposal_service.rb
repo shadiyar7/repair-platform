3e81714861df2da63afe2496e492ecc3d7612ad3
shadiyardesk <shadiyar@deskalerts.com>
2026-02-25 13:29:20 +0500

    fix: add missing director_name to CP service

--- backend/app/services/commercial_proposal_service.rb
+++ backend/app/services/commercial_proposal_service.rb
@@ -1,11 +1,12 @@
 class CommercialProposalService
-  def initialize(products_data, client_name: nil)
+  def initialize(products_data, client_name: nil, director_name: nil)
     @products = products_data
     @client_name = client_name
+    @director_name = director_name
   end
 
   def generate
-    generator = Pdf::CommercialProposalGenerator.new(@products, client_name: @client_name)
+    generator = Pdf::CommercialProposalGenerator.new(@products, client_name: @client_name, director_name: @director_name)
     generator.generate
   end
 end
