2799e0eff1d9bdf1fbd6f5089f492073c0cb3d93
shadiyardesk <shadiyar@deskalerts.com>
2026-02-26 15:04:00 +0500

    fix: revert warehouse_id_1c to body for 1C sync

--- backend/app/jobs/sync_stocks_job.rb
+++ backend/app/jobs/sync_stocks_job.rb
@@ -11,14 +11,10 @@ class SyncStocksJob < ApplicationJob
     # Use production URL as fallback if ENV is missing
     production_url = "https://1cstart.itsheff.cloud/komandeersykixo/hs/int/get_stocks"
     one_c_url = ENV.fetch('ONE_C_API_URL', production_url)
-    
-    # 1C Expects a GET request. For GET, it's better to pass warehouse_id in query params.
-    warehouse_id_1c = warehouse.external_id_1c.to_s.rjust(9, "0")
     uri = URI(one_c_url)
-    params = { warehouse_id_1c: warehouse_id_1c }
-    uri.query = URI.encode_www_form(params)
     
-    Rails.logger.info "🛰 [SyncStocksJob] Triggering 1C Pull from: #{uri}"
+    warehouse_id_1c = warehouse.external_id_1c.to_s.rjust(9, "0")
+    Rails.logger.info "🛰 [SyncStocksJob] Triggering 1C Pull from: #{one_c_url} with body ID: #{warehouse_id_1c}"
     
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
@@ -28,6 +24,11 @@ class SyncStocksJob < ApplicationJob
     
     # Authenticate: username="администратор", password=""
     request.basic_auth("администратор", "")
+    
+    # Body: {"warehouse_id_1c": "..."}
+    request.body = JSON.dump({
+      "warehouse_id_1c": warehouse_id_1c
+    })
 
     
     begin
