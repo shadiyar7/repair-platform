9f3aa3dff0544c163a10a40ec385a543891ea344
shadiyardesk <shadiyar@deskalerts.com>
2026-02-26 15:56:31 +0500

    fix: allow both render domains in config.hosts

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -96,10 +96,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "repair-platform.onrender.com",
+    "repair-platform-backend.onrender.com",
+    "dynamix.kz",
+    "www.dynamix.kz"
+  ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
